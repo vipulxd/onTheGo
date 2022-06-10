de484a49ef81810ba9b987d6643eedd8dfa547c7
vipul dev <vipul.dev@thoughtwizards.com>
2022-06-10 14:55:38 +0300

    Global font change

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+    {
+      path : 'overview',
+      loadChildren : ()=> import('./module/preview/preview.module').then(m => m.PreviewModule)
+    },
+    {
+        path: '',
+        redirectTo: 'overview',
+        pathMatch : 'full'
+    }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
